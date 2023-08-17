26c5213b0def281ea19082e1ea802e2009421531
Phillippe Caetano <ph.ufeb@gmail.com>
2023-08-17 19:22:53 -0300

    Implement wallet connection

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -20,12 +20,36 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState('');
+
   const checkIfWalletIsConnected = async () => {
     if (!ethereum) return alert('Please install metamask');
 
     const accounts = await ethereum.request({ method: 'eth_accounts' });
 
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+
+      // getAllTransactions();
+    } else {
+      console.log('No accounts found.');
+    }
+
     console.log(accounts)
+  }
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask');
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+
+      throw new Error("No ethereum object.");
+    }
   }
 
   useEffect(() => {
@@ -33,7 +57,7 @@ export const TransactionProvider = ({ children }) => {
   }, []);
 
   return (
-    <TransactionContext.Provider value={{ value: 'test' }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
       {children}
     </TransactionContext.Provider>
   );
